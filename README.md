8ab1593fa34eccae3f030af9d6a6d652030084fd
Dipali Gojre <dipalisag@gmail.com>
2021-07-18 17:10:44 +0530

    react app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Shopping Cart App
+
+## Project Images
+
+![Home](./images/home.png)
+
+![Product](./images/product.png)
+
+![Cart](./images/cart.png)
+
+Deployment Link: 
+https://shopping-cart-48f78b.netlify.app/
---
+++ src/reducers/reducer.products.js
@@ -0,0 +1,32 @@
+import products from '../product.json';
+
+export default function () {
+    return products;
+}
+
+
+// const intialState = {
+//     products: [],
+//   };
+  
+//   export const productsReducer = (state = intialState, { type, payload }) => {
+//     switch (type) {
+//       case ActionTypes.SET_PRODUCTS:
+//         return { ...state, products: payload };
+//       default:
+//         return state;
+//     }
+//   };
+  
+//   export const selectedProductsReducer = (state = {}, { type, payload }) => {
+//     console.log(type);
+//     switch (type) {
+//       case ActionTypes.SELECTED_PRODUCT:
+//         return { ...state, ...payload };
+//       case ActionTypes.REMOVE_SELECTED_PRODUCT:
+//         return {};
+//       default:
+//         return state;
+//     }
+//   };
+  
