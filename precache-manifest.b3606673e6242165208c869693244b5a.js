04ee2b550d622ffd0ba6bb0cbe0c7b81e5406830
Dipali Gojre <dipalisag@gmail.com>
2021-07-20 11:04:39 +0530

    Updates

---
+++ precache-manifest.b3606673e6242165208c869693244b5a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e9099863f5c86ead13e340b4524c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26bfcc4bdb8b9d0cfed1",
+    "url": "/static/css/2.fed4c95a.chunk.css"
+  },
+  {
+    "revision": "86c296e797b9223739b3",
+    "url": "/static/css/main.ecc204ab.chunk.css"
+  },
+  {
+    "revision": "26bfcc4bdb8b9d0cfed1",
+    "url": "/static/js/2.e3b38b62.chunk.js"
+  },
+  {
+    "revision": "86c296e797b9223739b3",
+    "url": "/static/js/main.72a73f1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
